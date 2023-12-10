dea700b1cd91d26b04f8602f6c5e55b5506b621f
Alberto <berto.olmo@gmail.com>
2023-12-10 01:30:07 +0100

    Actualizacion con sistema de busqueda en la tabla y footer y header vacios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatListModule} from '@angular/material/list';
+import { HeaderComponent } from "./header/header.component";
+import { MainTableComponent } from "./main-table/main-table.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatListModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MatListModule, HeaderComponent, MainTableComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'proyectoPFC';
