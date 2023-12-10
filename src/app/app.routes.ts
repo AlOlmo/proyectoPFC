3ea4085cf723edcd437a7599a3356af806c28551
Javier Olmo <olmo.injerto.javier@gmail.com>
2023-12-10 19:32:22 +0100

    Fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {MainTableComponent} from "./main-table/main-table.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'main-table', component: MainTableComponent },
+  { path: '**', redirectTo: '/main-table' },
+];
